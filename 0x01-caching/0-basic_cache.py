34fc8761c833cec0fdd4d240f9998d5feaa80dca
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-29 14:04:01 +0100

    feat: solve task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic cache class
+    """
+
+    def put(self, key, item):
+        """Assigns to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """returns the value in self.cache_data
+        linked to key
+        """
+
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
