7bd332e970d5df1e488778fc771de5b633a47fe3
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-29 14:41:04 +0100

    feat: solve task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""This module contains code for fifo cache
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO cache class
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.__queue = []
+
+    def put(self, key, item):
+        """Assigns to the dictionary self.cache_data
+        the item value for the key.
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.__queue) == self.MAX_ITEMS and key not in self.cache_data:
+            discard_key = self.__queue.pop(0)
+            del self.cache_data[discard_key]
+            print("DISCARD: {}".format(discard_key))
+
+        if key in self.cache_data:
+            key_index = self.__queue.index(key)
+            del self.__queue[key_index]
+
+        self.cache_data[key] = item
+        self.__queue.append(key)
+
+    def get(self, key):
+        """returns the value in self.cache_data
+        linked to key
+        """
+
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
